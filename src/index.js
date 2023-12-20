381b3a5f0112670dcec01a94195878aad7462a59
rohit shyam mane <rohitmane8356@gmail.com>
2023-12-20 16:47:06 +0530

    intial

--- src/index.js
+++ src/index.js
@@ -12,13 +12,14 @@ window.process = {};
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    
+    
     <Provider store={store}>
-    
+    <BrowserRouter>
     <App />
-    
-    </Provider>
     </BrowserRouter>
+    </Provider>
+    
   </React.StrictMode>
 );
 
